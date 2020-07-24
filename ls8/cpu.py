ba36d4ef6e0edb37c018192433c54d6096ee7d6b
Ous Bayaa <bayaaous@gmail.com>
2020-07-23 23:19:57 -0700

    mvp2 revised

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -18,6 +18,7 @@ class CPU:
         self.LDI = 0b10000010
         self.PRN = 0b01000111
         self.HLT = 0b00000001
+        self.MUL = 0b10100010
 
         # self.POP = 
         # self.PUSH =
@@ -36,7 +37,7 @@ class CPU:
         if len(sys.argv) != 2:
             print("usage: ls8.py filename")
             sys.exit(1)
-        
+
         try:
             with open(sys.argv[1]) as f:
                 for line in f:
@@ -52,19 +53,19 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+        #
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -98,4 +99,24 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        IR = 0
+
+        while True:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR == self.HLT:
+                exit()
+            elif IR == self.LDI: #Load value into register
+                self.ram_write(operand_a, operand_b)
+                self.pc += 3
+            elif IR == self.PRN: # Print
+                print(self.ram_read(operand_a))
+                self.pc += 2
+            elif IR == self.MUL: # Multiple
+                value = (self.ram_read(operand_a) * self.ram_read(operand_b)) & 0xFF
+                self.ram_write(operand_a, value)
+                self.pc += 3
+            else:
+                break
